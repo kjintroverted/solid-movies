d389c546fea44efd5fc6db2dee75b462d4b2b905
kjintroverted <wkgreen13@gmail.com>
2022-01-20 16:12:57 -0500

    adds styling for the move card header

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,17 +1,44 @@
-import { Card, CardHeader, Row } from "solid-core/dist/components/styled"
+import { Card, Column, Frame } from "solid-core/dist/components/styled"
+import styled from "styled-components";
+import { THEME } from "../util";
 
 const MovieList = ({ movies }) => {
   return (
-    <Row wrap="wrap">
+    <Container wrap="wrap">
       {
         movies.map(m => (
-          <Card key={ m.id }>
-            <CardHeader>{ m.data.Title }</CardHeader>
+          <Card className="movie" key={ m.id }>
+            <Frame
+              position='absolute'
+              fit='cover'
+              top='0' left='0'
+              width='100%' height='10em'
+              focusX='center' focusY='15%'
+            >
+              <img src={ m.data.Poster } alt={ `${ m.data.Title } Poster` } />
+            </Frame>
+            <Column height='9em' justify='flex-end'>
+              <Title>{ m.data.Title }</Title>
+            </Column>
           </Card>
         ))
       }
-    </Row>
+    </Container>
   )
 }
 
 export default MovieList;
+
+const Container = styled.div`
+  display: flex;
+  padding: 1em;
+  *.movie {
+    margin: .5em;
+  }
+`
+
+const Title = styled.h2`
+  margin: 0px;
+  padding: .1em;
+  background: ${ THEME.primary }DD;
+`
