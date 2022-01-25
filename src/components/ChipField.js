83084b0731471247449aea9d40f9aa75cc456289
kjintroverted <wkgreen13@gmail.com>
2022-01-25 10:30:06 -0500

    updates profile comp

--- src/components/ChipField.js
+++ src/components/ChipField.js
@@ -1,7 +1,6 @@
 import { Chip, TextField } from "@material-ui/core";
 import { useState } from "react";
 import { Row } from "solid-core";
-import { Spacer } from "solid-core/dist/components/styled";
 import { THEME } from "../util";
 
 function ChipField({ data, onSubmit, onDelete, showForm }) {
